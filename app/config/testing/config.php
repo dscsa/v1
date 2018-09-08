31708113c107003af849abfd0305c5691b269bd6
Omar <omarwsow@gmail.com>
2018-09-08 14:52:41 -0400

    last

--- app/config/testing/config.php
+++ app/config/testing/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'https://test.sirum.org/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
