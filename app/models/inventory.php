c4302d19c2029dca5644f4b1e95ae891caccf2cb
root <root@ip-10-229-6-159.us-west-1.compute.internal>
2019-05-20 20:20:26 +0000

    another test commit for test branch only

--- app/models/inventory.php
+++ app/models/inventory.php
@@ -111,6 +111,7 @@ class inventory extends MY_Model
 
 	//test comment as setting up new branch workflow
 	//TEST - only show this in test branch until we pull request
+	//TEST 2 - only on test branch again, after its already been pushed
 	//Should be depracated
 	function bulk($data)
 	{
