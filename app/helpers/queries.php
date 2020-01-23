0416e9f5f516b3a8b07b463bf3eafd950fe84573
akircher <adam@Adams-MacBook-Pro-2.local>
2020-01-22 16:48:19 -0800

    Some useful queries to keep around

--- app/helpers/queries.php
+++ app/helpers/queries.php
@@ -13,31 +13,6 @@ WHERE
 	donee_id = 1052 AND
 	YEAR(COALESCE(donation.date_shipped, donation.date_received, donation.date_verified, donation.created)) >= 2016;
 
--- Worthington —> CPC
-UPDATE
-	donation_items
-JOIN
-	donation ON donation.id = donation_id
-SET
-	donation_items.archived = 0,
-	donee_qty = 0.35 * donor_qty
-WHERE
-	donor_id = 744 AND
-	donee_id = 745 AND
-	YEAR(COALESCE(donation.date_shipped, donation.date_received, donation.date_verified, donation.created)) <= 2015;
-
-UPDATE
-	donation_items
-JOIN
-	donation ON donation.id = donation_id
-SET
-	donation_items.archived = 0,
-	donee_qty = 0.50 * donor_qty
-WHERE
-	donor_id = 744 AND
-	donee_id = 745 AND
-	YEAR(COALESCE(donation.date_shipped, donation.date_received, donation.date_verified, donation.created)) >= 2016;
-
 -- Westlake -> VPCP
 UPDATE
 	donation_items
@@ -74,3 +49,30 @@ SET
 WHERE
 	donor_id = 1291 AND
 	donee_id = 1052
+
+-- Worthington —> CPC
+/*
+UPDATE
+	donation_items
+JOIN
+	donation ON donation.id = donation_id
+SET
+	donation_items.archived = 0,
+	donee_qty = 0.35 * donor_qty
+WHERE
+	donor_id = 744 AND
+	donee_id = 745 AND
+	YEAR(COALESCE(donation.date_shipped, donation.date_received, donation.date_verified, donation.created)) <= 2015;
+
+UPDATE
+	donation_items
+JOIN
+	donation ON donation.id = donation_id
+SET
+	donation_items.archived = 0,
+	donee_qty = 0.50 * donor_qty
+WHERE
+	donor_id = 744 AND
+	donee_id = 745 AND
+	YEAR(COALESCE(donation.date_shipped, donation.date_received, donation.date_verified, donation.created)) >= 2016;
+*/
